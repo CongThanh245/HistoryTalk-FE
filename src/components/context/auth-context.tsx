907ce989696f623542fd6a26df4917ca4a2f9549
CongThanh245 <thanh7akt@gmail.com>
2026-02-13 10:26:48 +0700

    turn off context

--- src/components/context/auth-context.tsx
+++ src/components/context/auth-context.tsx
@@ -1,94 +1,106 @@
-"use client";
+// "use client";
 
-import { getUserProfile } from "@/app/api/user/action";
-import { UserProfile } from "@/features/user/models/response";
-import { useRouter } from "next/navigation";
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-  useCallback,
-} from "react";
+// import { getUserProfile } from "@/app/api/user/action";
+// import { UserProfile } from "@/features/user/models/response";
+// import { useRouter } from "next/navigation";
+// import {
+//   createContext,
+//   useContext,
+//   useEffect,
+//   useState,
+//   useCallback,
+// } from "react";
 
-type AuthContextType = {
-  user: UserProfile | null;
-  isAuthenticated: boolean;
-  logout: () => void;
-  setAuthenticated: (value: boolean) => void;
-  refetch: () => Promise<void>;
-};
+// type AuthContextType = {
+//   user: UserProfile | null;
+//   isAuthenticated: boolean;
+//   logout: () => void;
+//   setAuthenticated: (value: boolean) => void;
+//   refetch: () => Promise<void>;
+// };
 
-const AuthContext = createContext<AuthContextType>({
-  user: null,
-  isAuthenticated: false,
-  logout: () => {},
-  setAuthenticated: () => {},
-  refetch: async () => {},
-});
+// const AuthContext = createContext<AuthContextType>({
+//   user: null,
+//   isAuthenticated: false,
+//   logout: () => {},
+//   setAuthenticated: () => {},
+//   refetch: async () => {},
+// });
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<UserProfile | null>(null);
-  const router = useRouter();
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+// export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+//   const [user, setUser] = useState<UserProfile | null>(null);
+//   const router = useRouter();
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const logout = async () => {
-    try {
-      const response = await fetch("/api/auth", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+//   const logout = async () => {
+//     try {
+//       const response = await fetch("/api/auth", {
+//         method: "DELETE",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
 
-      if (response.ok) {
-        setIsAuthenticated(false);
-        setUser(null);
+//       if (response.ok) {
+//         setIsAuthenticated(false);
+//         setUser(null);
 
-        router.push("/");
-      } else {
-        console.error("Logout failed");
-      }
-    } catch (error) {
-      console.error("Error during logout:", error);
-    }
-  };
+//         router.push("/");
+//       } else {
+//         console.error("Logout failed");
+//       }
+//     } catch (error) {
+//       console.error("Error during logout:", error);
+//     }
+//   };
 
-  const checkAuth = useCallback(async () => {
-    const response = await getUserProfile();
+//   const checkAuth = useCallback(async () => {
+//     const response = await getUserProfile();
 
-    console.log("fetching user profile:", response);
+//     console.log("fetching user profile:", response);
 
-    if (response.isSuccess && response.value) {
-      setUser(response.value);
-      setIsAuthenticated(true);
-    } else {
-      setUser(null);
-      setIsAuthenticated(false);
-    }
-  }, []);
+//     if (response.isSuccess && response.value) {
+//       setUser(response.value);
+//       setIsAuthenticated(true);
+//     } else {
+//       setUser(null);
+//       setIsAuthenticated(false);
+//     }
+//   }, []);
 
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
+//   useEffect(() => {
+//     checkAuth();
+//   }, [checkAuth]);
 
-  const setAuthenticated = (value: boolean) => {
-    setIsAuthenticated(value);
-  };
+//   const setAuthenticated = (value: boolean) => {
+//     setIsAuthenticated(value);
+//   };
 
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         isAuthenticated,
+//         logout,
+//         setAuthenticated,
+//         refetch: checkAuth,
+//         user,
+//       }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export const useAuthContext = () => useContext(AuthContext);
+
+
+import React from 'react'
+
+function AuthContext(): React.ReactNode {
   return (
-    <AuthContext.Provider
-      value={{
-        isAuthenticated,
-        logout,
-        setAuthenticated,
-        refetch: checkAuth,
-        user,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-};
+    <div>auth-context</div>
+  )
+}
 
-export const useAuthContext = () => useContext(AuthContext);
+export default AuthContext;
+
